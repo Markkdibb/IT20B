9fea6fda3ef5686271037e91872441cad37af5c5
markkdibb <marcdavetangaro@gmail.com>
2025-11-21 08:31:07 +0800

    Added while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,53 @@
+/*
+i = 5;
+
+while (i>=1){
+
+    console.log(i);
+    i--;
+}
+    */
+/*
+i=1;
+
+while (i<=5){
+    console.log(i);
+    i++;
+}
+    */
+/*
+output = "*";
+i =1;
+
+while (i<=5){
+    output += i +" ";
+    i++;
+}
+
+console.log(i);
+
+*/
+/*
+output = "";
+i =5;
+
+while (i>=1){
+    output += i +" ";
+    i--;
+}
+
+console.log(i);
+*/
+
+arr=[5,8,6];
+output = " ";
+i=0;
+
+while(i<=arr.length-1){
+    output += arr[i] + " ";
+i++;
+}
+
+console.log(output);
+
+
