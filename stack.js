0e9562c8e68b96d8d60aaba65709b2488f54e0f8
markkdibb <marcdavetangaro0@gmail.com>
2025-11-25 08:55:21 +0800

    Added pop method for stack.js

--- stack.js
+++ stack.js
@@ -30,6 +30,17 @@ class Stack{
 
         return this.data [this.topIndex];
     }
+
+    pop(){
+        //Correcting Statement
+        if(this.is_empty()) return null;
+
+        let value = this.data[this.topIndex];
+        delete this.data[this.topIndex];
+        this.topIndex--;
+        return value;
+
+    }
 }
 
 //
@@ -40,6 +51,8 @@ stack1.push(2);
 stack1.push(3);
 stack1.push(5);
 stack1.push(8);
-console.log(stack1.is_empty());
-console.log(stack1.size());
-console.log("The top element is " + stack1.peek());
+console.log("The Current size is " + stack1.size());
+console.log("The topt elemennt is " + stack1.pop());
+console.log("Top element " + stack1.pop() + "is popped, the new top element is" + stack1.peek());
+console.log("The new stack size is " + stack1.size());
+
