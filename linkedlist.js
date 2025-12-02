361733f6ed52c34a50732217e3733ee0248bd22d
Markkdibb <marcdavetangaro0@gmail.com>
2025-12-02 08:37:33 +0800

    Added utility methods for linkedlist.js

---
+++ linkedlist.js
@@ -0,0 +1,60 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+
+    constructor(){
+        this.head = null;
+
+    }
+
+    insert_at_beginning(element){
+        const node = new Node(element);
+        node.next = this.head;
+        this.head = node;
+    }
+
+    traverse(){
+        let current = this.head;
+        let output = "";
+
+        while(current){
+            output += current.value + " ";
+            current = current.next;
+
+
+        }
+
+        return output;
+
+    }
+
+    insert_at_end(element){
+        const node = new Node(element);
+
+        if(!this.head){
+            this.head = node;
+            return;
+        
+        }
+
+        let current = this.head;
+
+        while(current.next){
+            current.next
+        }
+            
+
+
+    }
+}
+
+let list1 = new LinkedList();
+list1.insert_at_beginning(5);
+list1.insert_at_beginning(3);
+list1.insert_at_beginning(1);
+console.log(list1.traverse());
